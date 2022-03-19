683d7cb49e9ded0033cf7a5756375ae061ef76fd
Julienne Garcia <jkgdev8@gmail.com>
2022-03-18 22:24:00 -0700

    routes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,9 @@
-//21.3.5
-
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
+const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
 
-
-const { typeDefs, resolvers } = require('./schemas');
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -16,7 +13,7 @@ const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: authMiddleware
+    context: authMiddleware,
   });
   await server.start();
   server.applyMiddleware({ app });
